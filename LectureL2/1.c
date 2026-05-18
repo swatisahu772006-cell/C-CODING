cf62e4994a0cece2d6555ec78dcd1d593c0ddee2
swati sahu <swatisahu772006@gmail.com>
2026-05-18 08:05:01 +0530

    add 1.c

---
+++ LectureL2/1.c
@@ -0,0 +1,14 @@
+// CHECK ADULT OR NOTADULT
+
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter age : ");
+    scanf("%d",&age);
+    if(age>18){
+        printf("adult\n");
+    printf("they can drive and vote");}
+    else{
+        printf("not adult");
+    } 
