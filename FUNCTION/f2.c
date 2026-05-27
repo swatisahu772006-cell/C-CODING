cd2a07f46b0da2456e09526552a9f1602ea6ac0f
swati sahu <swatisahu772006@gmail.com>
2026-05-27 21:47:16 +0530

    add f2.c

---
+++ FUNCTION/f2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+char ch;
+    printf("enter i for indian and f for france :");
+    scanf("%d",&ch);
+    if(ch =='i')
+    {
+    printf("namesta\n");
+    }
+    if(ch =='f'){
+        printf("bonjour");
+    }
+
+}
