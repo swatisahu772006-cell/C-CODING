141b6e21ae9d25bcd90fc49b98b92cef6f99c5cc
swati sahu <swatisahu772006@gmail.com>
2026-06-01 23:25:06 +0530

    add r7.c

---
+++ RECURSION/r7.c
@@ -0,0 +1,22 @@
+// wap to print the fibonacci sequence/ nth term of the fibonacci sequence
+#include<stdio.h>
+int fib(int n);
+
+int main(){ 
+    int n;
+    printf("fib series:%d\n",fib(6));
+    return 0;
+}
+int fib(int n){ 
+    if (n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+     }
+    int fibNm1=fib(n-1);
+    int fibNm2=fib(n-2);
+    int fibN=fibNm1+fibNm2;
+    //printf("fib of %d is:%d\n",n,fibN);
+    return fibN;
+}    
